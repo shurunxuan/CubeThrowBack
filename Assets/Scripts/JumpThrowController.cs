250725d2188f0ed0c5d739ce849ae266f674d5b3
Victor Shu (RIT Student) <vs1571@ad.rit.edu>
2019-02-23 17:51:08 -0500

    Controller setting

---
+++ Assets/Scripts/JumpThrowController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpThrowController : MonoBehaviour {
+
+    // Set by PlayerController
+    [HideInInspector]
+    public bool JumpThrow;
+
+    public float JumpForce;
+
+
+    private Rigidbody playerRigidbody;
+    // Use this for initialization
+    void Start () {
+        playerRigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (JumpThrow)
+        {
+            playerRigidbody.AddForce(Vector3.up * JumpForce);
+            JumpThrow = false;
+        }
+	}
+}
---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour {
+
+    // Set by PlayerController
+    [HideInInspector]
+    public float Horizontal;
+    [HideInInspector]
+    public float Vertical;
+
+
+    public float MovementSpeed;
+
+    private Vector3 cameraForward;
+    private Vector3 cameraRight;
+    // Use this for initialization
+    void Start () {
+        cameraForward = Vector3.Cross(Camera.main.transform.right, Vector3.up);
+        cameraRight = Vector3.Cross(Vector3.up, cameraForward);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += (cameraForward * Vertical + cameraRight * Horizontal) * MovementSpeed;
+    }
+}
