c860638e836162b822c8a10be85ffed596c28893
Mark DelFavero <markadelf@gmail.com>
2019-02-23 16:39:21 -0500

    Jump parenting

---
+++ Assets/Scripts/Robot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot : MonoBehaviour {
+    public float speed;
+    public Collider hitbox;
+
+    public CharacterController control;
+
+	// Use this for initialization
+	void Start () {
+        control = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
