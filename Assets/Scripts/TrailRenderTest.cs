596fde794dd4ea5b7fe98e188c00e45ff3cefe62
Ollyting Xin (RIT Student) <ox7313@ad.rit.edu>
2019-02-24 17:21:56 -0500

    uploaded tail effect

---
+++ Assets/Scripts/TrailRenderTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailRenderTest : MonoBehaviour {
+
+	// Use this for initialization
+
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = new Vector3(10.0f * Mathf.Sin(Time.time), 0.0f, 0.0f);
+	}
+}
