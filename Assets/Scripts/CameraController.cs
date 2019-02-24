eba7d33373fb51da6673e6c4fcd65e30dcddb06a
Runxuan Shu <shurunxuan@hotmail.com>
2019-02-24 17:04:39 -0500

    Fixed camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,9 +8,7 @@ public class CameraController : MonoBehaviour
 {
 
     public List<Transform> Tracking;
-
-    private Vector3 center;
-
+    
     // Use this for initialization
     void Start()
     {
@@ -20,26 +18,30 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 viewportBBMin = new Vector2(Single.MaxValue, Single.MaxValue);
-        Vector2 viewportBBMax = new Vector2(Single.MinValue, Single.MinValue);
+        Vector3 viewportBBMin = new Vector2(Single.MaxValue, Single.MaxValue);
+        Vector3 viewportBBMax = new Vector2(Single.MinValue, Single.MinValue);
         foreach (var transform1 in Tracking)
         {
             Vector3 screenPosition = Camera.main.WorldToViewportPoint(transform1.position);
             if (screenPosition.x < viewportBBMin.x) viewportBBMin.x = screenPosition.x;
             if (screenPosition.y < viewportBBMin.y) viewportBBMin.y = screenPosition.y;
+            if (screenPosition.z < viewportBBMin.z) viewportBBMin.z = screenPosition.z;
             if (screenPosition.x > viewportBBMax.x) viewportBBMax.x = screenPosition.x;
             if (screenPosition.y > viewportBBMax.y) viewportBBMax.y = screenPosition.y;
-            center += transform1.position;
+            if (screenPosition.z > viewportBBMax.z) viewportBBMax.z = screenPosition.z;
         }
 
-        center /= Tracking.Count;
-        float newSize = (viewportBBMax - viewportBBMin).magnitude;
+        Vector2 viewportSizeMin = new Vector2(viewportBBMin.x, viewportBBMin.y);
+        Vector2 viewportSizeMax = new Vector2(viewportBBMax.x, viewportBBMax.y);
+        float newSize = (viewportSizeMin - viewportSizeMax).magnitude;
+        Vector3 newViewportCenter = (viewportBBMin + viewportBBMax) / 2.0f;
+        Vector3 worldCenter = Camera.main.ViewportToWorldPoint(newViewportCenter);
         float ratio = newSize * 2.0f;
 
         Camera.main.orthographicSize *= ratio;
 
 
-        transform.position = center - transform.forward * 500.0f;
+        transform.position = worldCenter - transform.forward * 150.0f;
 
     }
 }
