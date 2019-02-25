34df7f70369e6a02768693bc91a948c6a8dc5aa2
OllytingXin <42716913+OllytingXin@users.noreply.github.com>
2019-02-25 18:10:25 -0500

    done

---
+++ Assets/Scripts/DestroyDoorTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyDoorTest : MonoBehaviour {
+
+    public GameObject breakdownPrefab;
+    public GameObject doorObj;
+
+    // Update is called once per frame
+    void Start()
+    {
+        
+    }
+    void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 effectPos = new Vector3(doorObj.transform.position.x, doorObj.transform.position.y, doorObj.transform.position.z);
+            Vector3 effectRot = new Vector3(doorObj.transform.rotation.x, doorObj.transform.rotation.y, doorObj.transform.rotation.z);
+            Destroy(doorObj.gameObject);
+            GameObject breakDownEffect = Instantiate(breakdownPrefab, effectPos, Quaternion.identity) as GameObject; 
+        }
+	}
+}
