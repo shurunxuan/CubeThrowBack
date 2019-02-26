a7b3748e4e6ff23fdfe34df7ecec9e744cb5fab7
Israel Anthony <ixa4371@rit.edu>
2019-02-25 23:32:51 -0500

    Added door objective and timer

---
+++ Assets/Scripts/ObjectivesController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectivesController : MonoBehaviour
+{
+    public int doorHealth = 20;
+
+    private float targetTime = 60.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // Check if the door is smashed or if the time has run out
+        if (doorHealth <= 0)
+        {
+            GameObject.Find("HealthBar").gameObject.transform.localScale = Vector3.zero;
+            GameObject.Find("GameOverText").GetComponent<UnityEngine.UI.Text>().text = "ROBOT IS VICTORIOUS!";
+            Time.timeScale = 0;
+        }
+        if (targetTime <= 0)
+        {
+            GameObject.Find("GameOverText").GetComponent<UnityEngine.UI.Text>().text = "TIMES UP!";
+            Time.timeScale = 0;
+        }
+
+        // Update time
+        targetTime -= Time.deltaTime;
+        int minutesTime = (int)targetTime / 60;
+        int secondsTime = (int)targetTime % 60;
+        if(targetTime > 60)
+        {
+
+            if (secondsTime < 10)
+            {
+                string correctedSecondsTime = "0" + secondsTime.ToString();
+                GameObject.Find("TimerText").GetComponent<UnityEngine.UI.Text>().text = minutesTime + ":" + correctedSecondsTime;
+            }
+            else if (secondsTime > 10)
+            {
+                GameObject.Find("TimerText").GetComponent<UnityEngine.UI.Text>().text = minutesTime + ":" + secondsTime;
+            }
+        }
+        else if(targetTime <= 60.0f)
+        {
+            int seconds = (int)targetTime;
+            if (secondsTime < 10)
+            {
+                string correctedSecondsTime = "0" + seconds.ToString();
+                GameObject.Find("TimerText").GetComponent<UnityEngine.UI.Text>().text = "0:" + correctedSecondsTime;
+            }
+            else if (secondsTime > 10)
+            {
+                GameObject.Find("TimerText").GetComponent<UnityEngine.UI.Text>().text = "0:" + seconds.ToString();
+            }
+            
+        }
+
+        // Update health bar
+        Vector3 healthScale = GameObject.Find("HealthBar").gameObject.transform.localScale;
+        healthScale.x = (doorHealth) / 20.0f;
+        GameObject.Find("HealthBar").gameObject.transform.localScale = healthScale;
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.name.Contains("HitboxSpot")) return;
+
+        // Get Damage
+        doorHealth--;
+    }
+}
